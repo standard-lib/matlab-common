c35ee83685d39013f03e2fea88325d76c3d92b14
standard-lib <standard_lib@yahoo.co.jp>
2026-08-03 17:11:02 +0900

    delete old files

--- +stat/compcov.m
+++
@@ -1,7 +0,0 @@
-function [S] = compcov(z)
-%COMPCOV 複素数ベクトルの実部と虚部を2つの確率変数とする共分散行列の不偏推定
-%   実部と虚部がそれぞれ非独立な正規分布であることを仮定している．
-    z = reshape(z,[],1);
-    zarr = [real(z),imag(z)];
-    S = cov(zarr);
-end
